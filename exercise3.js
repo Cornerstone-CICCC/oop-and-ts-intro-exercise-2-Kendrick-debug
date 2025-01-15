9a64f6a97e9f68caeb0adc1c616b929204bfdd7a
Philip Yaw Boahene <boahenep22@gmail.com>
2025-01-15 12:33:16 -0800

    oop exe 2 completed

--- exercise3.js
+++ exercise3.js
@@ -6,16 +6,36 @@
 // 4. In the `Manager` class, use `super` to call the parent constructor, and override the `introduce()` method to include the department in the output.
 //    The output should be: "Hi, I'm [name], a [position] in the [department] department."
 
-
 class Employee {
   // YOUR CODE HERE
+  #name;
+  #position;
+
+  constructor(name, position) {
+    this.firstname = name;
+    this.position = position;
+  }
+
+  introduce() {
+    return `Hi, I'm ${this.#name}, a ${this.#position}`;
+  }
 }
 
 class Manager extends Employee {
   // YOUR CODE HERE
-}
+  #department;
 
+  constructor(name, position, dapartment) {
+    super(name, position);
+    this.#department = dapartment;
+  }
+  introduce() {
+    return `Hi, I'm ${this.firstname}, a ${this.position} in the ${
+      this.#department
+    } department`;
+  }
+}
 
 // Test Code / Driver Code
 const manager = new Manager("Alice", "Manager", "Sales");
-console.log(manager.introduce());  // Expected Output: "Hi, I'm Alice, a Manager in the Sales department"
+console.log(manager.introduce()); // Expected Output: "Hi, I'm Alice, a Manager in the Sales department"
